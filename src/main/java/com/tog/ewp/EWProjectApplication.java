64029967c99dea51ef0b3074f191310a8c27bd83
devilasu <73906066+devilasu@users.noreply.github.com>
2023-02-25 20:00:20 +0900

    first commit

---
+++ src/main/java/com/tog/ewp/EWProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tog.ewp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tog/ewp/EWProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tog.ewp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
