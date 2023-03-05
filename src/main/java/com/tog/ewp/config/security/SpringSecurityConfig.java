a9b4bed296992787de3afa56f49c5e4165eaf28e
devilasu <73906066+devilasu@users.noreply.github.com>
2023-03-05 19:25:03 +0900

    시큐리티 켠 상태로 h2콘솔 접속 가능하게 수정

--- src/main/java/com/tog/ewp/config/security/SpringSecurityConfig.java
+++ src/main/java/com/tog/ewp/config/security/SpringSecurityConfig.java
@@ -1,42 +1,48 @@
 /**
-* @author			: YJ Kim
-* @date				: 2023.02.26
-* @description		: 스프링시큐리티 설정
-* ===========================================================
-* DATE					AUTHOR				NOTE
-* -----------------------------------------------------------
-* 2023.02.26				YJ Kim				최초 생성
-*/
-package com.tog.ewp.config.security;
+ * @author : YJ Kim
+ * @date : 2023.02.26
+ * @description : 스프링시큐리티 설정
+ *              =========================================================== DATE
+ *              AUTHOR NOTE
+ *              -----------------------------------------------------------
+ *              2023.02.26 YJ Kim 최초 생성
+ */
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfig{
-	
-	/**
-	* @methodName    : passwordEncoder
-	* @author        : YJ Kim
-	* @date          : 2023.02.26
-	* @description   : 비밀번호 해시 암호화
-	*/
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Bean
-	public SecurityFilterChain configure(HttpSecurity http) throws Exception{
-		http
-		.authorizeHttpRequests()
-		.anyRequest().permitAll();
-		return http.build();
-	}
-}
+  package com.tog.ewp.config.security;
+  
+  import org.springframework.context.annotation.Bean;
+  import org.springframework.context.annotation.Configuration;
+  import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+  import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+  import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+  import org.springframework.security.web.SecurityFilterChain;
+  
+  
+  @Configuration
+  @EnableWebSecurity
+  public class SpringSecurityConfig{
+  
+	 /**
+	 * @methodName : passwordEncoder
+	 * @author : YJ Kim
+	 * @date : 2023.02.26
+	 * @description : 비밀번호 해시 암호화
+	 */
+	  @Bean
+	  public BCryptPasswordEncoder passwordEncoder() {
+		  return new BCryptPasswordEncoder();
+	  }
+	  
+	  @Bean
+	  public SecurityFilterChain configure(HttpSecurity http) throws Exception{
+		  http .authorizeHttpRequests() .anyRequest().permitAll()
+		  
+		  .and()
+		  .headers() .frameOptions() .disable()
+		  
+		  .and()
+		  .csrf().disable();
+		  return http.build();
+	  } 
+  }
+ 
